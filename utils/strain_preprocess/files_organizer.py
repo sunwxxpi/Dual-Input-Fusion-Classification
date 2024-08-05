597b6dc5e4d5345ef15637940a808fdd35474720
Sunwoo Pi <ppssww1202@naver.com>
2024-08-05 04:46:56 +0000

    refactor: Organize files by first alphabetic character or by whether they contain the character "E" in their filenames

--- utils/strain_preprocess/files_organizer.py
+++ utils/strain_preprocess/files_organizer.py
@@ -1,8 +1,13 @@
 import os
-import shutil
+import glob
 import re
+import shutil
 
-def organize_files(base_dir):
+
+def organize_files_by_alpha(base_dir):
+    """
+    Organizes files into directories based on the first alphabetical character in their filenames.
+    """
     categories = ['G', 'S']
     scores = ['score1', 'score2', 'score3', 'score4', 'score5']
     
@@ -27,5 +32,45 @@ def organize_files(base_dir):
                             print(f"Copied {source_path} to {dest_path}")
 
 
-base_directory = './strain_png'
-organize_files(base_directory)
+def move_files_containing_E(base_dir):
+    """
+    Moves files with 'E' in their filenames from SCORE 1 to SCORE 5 to their respective directories.
+    """
+    output_dir = os.path.join(base_dir, "S/score")
+
+    for score in range(1, 6):
+        output_path = os.path.join(output_dir + str(score))
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+
+        input_path = os.path.join(base_dir, f"SCORE {score}", "**", "*E*.png")
+        images = glob.glob(input_path, recursive=True)
+
+        for image_path in images:
+            file_name = os.path.basename(image_path)
+            output_path = os.path.join(output_dir + str(score), file_name)
+            shutil.move(image_path, output_path)
+            print(f"Moved {image_path} to {output_path}")
+        
+    print("All file moves are complete.")
+
+
+def main():
+    base_directory = './strain_png'
+
+    print("Select an option:")
+    print("1. Organize files by first alphabetical character (G/S).")
+    print("2. Move files containing 'E' in filename from SCORE 1 to SCORE 5.")
+    
+    choice = input("Enter the number of your choice (1 or 2): ")
+
+    if choice == '1':
+        organize_files_by_alpha(base_directory)
+    elif choice == '2':
+        move_files_containing_E(base_directory)
+    else:
+        print("Invalid choice. Please enter 1 or 2.")
+
+
+if __name__ == "__main__":
+    main()
